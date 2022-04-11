fab7a516e8aa8da6745889b2f2446d09272113cf
DeadActive <undead.active@gmail.com>
2022-04-11 14:29:52 +0300

    fix

--- pages/policy.vue
+++ pages/policy.vue
@@ -292,3 +292,18 @@ export default {
     },
 };
 </script>
+
+<style>
+.hero {
+    background: #000;
+
+    @media (min-width: 768px) {
+        background: linear-gradient(
+                218deg,
+                rgba(0, 0, 0, 0) 59.18%,
+                #18d3d8 116.45%
+            ),
+            linear-gradient(288deg, #000000 63.57%, #7f0dc6 120.18%);
+    }
+}
+</style>
