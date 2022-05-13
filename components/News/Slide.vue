fcf244d7938b463cea05406e2bc2a88535f75fe5
DeadActive <undead.active@gmail.com>
2022-05-13 12:33:16 +0300

    fix

--- components/News/Slide.vue
+++ components/News/Slide.vue
@@ -1,8 +1,10 @@
 <template>
     <nuxt-link :to="content.path">
-        <div class="flex flex-col p-6 bg-white text-black rounded-lg mx-4 mb-8">
+        <div
+            class="flex flex-col p-6 bg-white text-black rounded-lg mx-4 mb-8 h-[500px]"
+        >
             <img :src="content.img" alt="" class="min-h-[135px] aspect-[3/2]" />
-            <span class="text-lg mt-6 h-36 overflow-hidden">{{
+            <span class="text-lg mt-6 h-36 overflow-hidden flex-grow">{{
                 content.title
             }}</span>
             <span class="text-lg mt-2 text-gray-500">{{
